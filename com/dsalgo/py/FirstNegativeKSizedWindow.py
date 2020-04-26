6f372b03ba4087fffa0b30e7b3cb1bd46372855b
Arjun Lather <arjunslather@gmail.com>
2020-04-26 16:38:29 +0530

    Add the question 'First negative element in k size window'

---
+++ com/dsalgo/py/FirstNegativeKSizedWindow.py
@@ -0,0 +1,19 @@
+def printFirstNegativeInteger(arr, k):
+    firstNegativeIndex = 0
+
+    for i in range(k - 1, len(arr)):
+
+        # skip out of window and positive elements
+        while firstNegativeIndex < i and (firstNegativeIndex <= i - k or arr[firstNegativeIndex] > 0):
+            firstNegativeIndex += 1
+
+        firstNegativeElement = arr[firstNegativeIndex] if arr[firstNegativeIndex] < 0 else 0
+        print(firstNegativeElement, end=' ')
+
+
+if __name__ == "__main__":
+    arr = [12, -1, -7, 8, -15, 30, 16, 28]
+    k = 3
+    printFirstNegativeInteger(arr, k)
+
+# contributed by Arjun Lather
