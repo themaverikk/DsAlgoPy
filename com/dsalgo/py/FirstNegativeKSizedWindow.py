a5103d8fe6930c1d043a0546cd6498f1b80fc1c5
Arjun Lather <arjunslather@gmail.com>
2020-05-03 13:30:29 +0530

    Simplify First negative in K sized window

--- com/dsalgo/py/FirstNegativeKSizedWindow.py
+++ com/dsalgo/py/FirstNegativeKSizedWindow.py
@@ -7,6 +7,7 @@ def printFirstNegativeInteger(arr, k):
         while firstNegativeIndex < i and (firstNegativeIndex <= i - k or arr[firstNegativeIndex] > 0):
             firstNegativeIndex += 1
 
+        # check if a negative element is found, otherwise use 0
         firstNegativeElement = arr[firstNegativeIndex] if arr[firstNegativeIndex] < 0 else 0
         print(firstNegativeElement, end=' ')
 
